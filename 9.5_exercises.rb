e2e558e14bf9c7182493754ffeffc7df674b7a8c
Jonathan Sevold <Jmsevold@gmail.com>
2014-04-07 09:27:04 -0700

    fixed indentation

--- 9.5_exercises.rb
+++ 9.5_exercises.rb
@@ -3,18 +3,18 @@
 # the answer variable. You’ll have to use return to exit from the loop.
 
 def ask(question)
-	puts question
+  puts question
   reply = gets.chomp.downcase
   while true
-	  if reply == 'yes'
-	  	return true
-	  elsif reply == 'no'
-	  	return false
-	  else
-	  	puts 'Please answer "yes" or "no"'
-	  	reply = gets.chomp.downcase
-	  end
-	end
+    if reply == 'yes'
+      return true
+    elsif reply == 'no'
+      return false
+    else
+      puts 'Please answer "yes" or "no"'
+      reply = gets.chomp.downcase
+    end
+  end
 end
 
 
@@ -22,32 +22,30 @@ end
 
 
 def old_roman_numeral(target)
-	num = target
-	numerals = %w{ M D C L X V I }
-	values = [1000,500,100,50,10,5,1]
-	result = ""
-	values.each_with_index do |val,index|
-		count = num / val
-		count.times { result += numerals[index] }
-		num -= val * count
-	end
-	result
+  num = target
+  numerals = %w{ M D C L X V I }
+  values = [1000,500,100,50,10,5,1]
+  result = ""
+  values.each_with_index do |val,index|
+    count = num / val
+    count.times { result += numerals[index] }
+    num -= val * count
+  end
+  result
 end
 
-
 ################# now with a hash
-
   
-  def old_roman_numeral(target)
-	num = target
-	numerals = { 'M' => 1000, 'D' => 500, 'C' => 100, 'L' => 50, 'X' => 10, 'V' => 5, 'I'=> 1 }
-	result = ""
-	numerals.each do |key,val|
-		count = num / val
-		count.times { result += key }
-		num -= val * count
-	end
-	result
+def old_roman_numeral(target)
+  num = target
+  numerals = { 'M' => 1000, 'D' => 500, 'C' => 100, 'L' => 50, 'X' => 10, 'V' => 5, 'I'=> 1 }
+  result = ""
+  numerals.each do |key,val|
+    count = num / val
+    count.times { result += key }
+    num -= val * count
+  end
+  result
 end
 
 
