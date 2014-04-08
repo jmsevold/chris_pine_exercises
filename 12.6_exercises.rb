6e00bd269e2e75f4344f5a78aca466d281407ac1
Jonathan Sevold <Jmsevold@gmail.com>
2014-04-08 12:09:42 -0700

    removed unused var in roman_to_integer

--- 12.6_exercises.rb
+++ 12.6_exercises.rb
@@ -1,7 +1,6 @@
 
 def roman_to_integer(roman_numeral)
   numerals = { 'M' => 1000, 'D' => 500, 'C' => 100, 'L' => 50, 'X' => 10, 'V' => 5, 'I'=> 1 }
-  order = %w{ M D C L X V I }
   num = 0
   roman_numeral.each_char do |char|
     num += numerals[char]
