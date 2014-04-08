1dddae3779324d7b13fe2539d8ac5c879a73ca3f
Jonathan Sevold <Jmsevold@gmail.com>
2014-04-08 12:28:59 -0700

    checked for invalid input in roman_to_integer

--- 12.6_exercises.rb
+++ 12.6_exercises.rb
@@ -1,11 +1,30 @@
 
 def roman_to_integer(roman_numeral)
   numerals = { 'M' => 1000, 'D' => 500, 'C' => 100, 'L' => 50, 'X' => 10, 'V' => 5, 'I'=> 1 }
-  num = 0
-  roman_numeral.each_char do |char|
-    num += numerals[char]
+  if valid_numeral?(roman_numeral)
+    num = 0
+    roman_numeral.each_char do |char|
+      num += numerals[char]
+    end
+  else
+    puts "Error: Not a valid roman numeral"
+    return
   end
  num
 end
 
+def valid_numeral?(numeral)
+  rn = %w{ M D C L X V I }
+  numeral.each_char do |char|
+    if !(rn.include?(char))
+      return false
+    end
+  end
+  true
+end
+
+
 roman_to_integer('LXXXIII') # => 83
+
+
+def birthday_helper
